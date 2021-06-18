1dbd32a2303aa8072adb52d45663674ced2098cf
Busayo Famutimi <famutimi.busayo@gmail.com>
2021-06-18 12:08:09 +0100

    Setup postgres database

--- 1 - Injection_Flaws/main.cpp
+++ 1 - Injection_Flaws/main.cpp
@@ -1,9 +1,18 @@
-/* #include <crow_all.h>
+#include <string>
+
+//#include "crow_all.h"
 #include <pqxx/pqxx>
 
 int main(int argc, char *argv[])
 {
-    crow::SimpleApp app;
-
-
-} */
+    //crow::SimpleApp app;
+    //psql-userdb="host=<host> dbname=<database-name> user=<database-user> password=<database-user-password> connect_timeout=30"
+    std::string databaseHost{"postgres-db"};
+    std::string databaseName{std::getenv("POSTGRES_DB")};
+    std::string databaseUser{std::getenv("POSTGRES_USER")};
+    std::string databasePassword{std::getenv("POSTGRES_PASSWORD")};
+    std::string databaseConnectionString{
+        "host=" + databaseHost + " dbname=" + databaseName + " user=" + databaseUser + " password=" + databasePassword + " connect_timeout=30"
+    };
+    pqxx::connection conn{databaseConnectionString};
+}
