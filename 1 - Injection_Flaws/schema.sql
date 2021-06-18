182840b84009598d4e45fa2c0ce6c9e339479207
Busayo Famutimi <famutimi.busayo@gmail.com>
2021-06-18 20:15:10 +0100

    Updated Injection_Flaws

--- 1 - Injection_Flaws/schema.sql
+++ 1 - Injection_Flaws/schema.sql
@@ -1,55 +1,16 @@
-
-CREATE TABLE turnusers_lt (
-    realm varchar(127) default '',
-    name varchar(512),
-    hmackey char(128),
-    PRIMARY KEY (realm,name)
+CREATE TABLE gtworld_users (
+	account_number INTEGER NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	middle_name VARCHAR(50) DEFAULT '',
+	last_name VARCHAR(50) NOT NULL,
+	card_pan VARCHAR(30) UNIQUE NOT NULL,
+	PRIMARY KEY (account_number)
 );
 
-CREATE TABLE turn_secret (
-	realm varchar(127) default '',
-	value varchar(256),
-	primary key (realm,value)
-);
+INSERT INTO gtworld_users (account_number, first_name, last_name, card_pan) VALUES(1234567890, 'Drake','Afolabi','1234-****-****-3456');
+INSERT INTO gtworld_users (account_number, first_name, last_name, card_pan) VALUES(1234567891, 'JoeBoy','Irawolowo','1234-****-****-3457');
+INSERT INTO gtworld_users (account_number, first_name, last_name, card_pan) VALUES(1234567892, 'Olamide','Daoudu','1234-****-****-3458');
+INSERT INTO gtworld_users (account_number, first_name, last_name, card_pan) VALUES(1234567893, 'Adekunle','Bronzeli','1234-****-****-3459');
+INSERT INTO gtworld_users (account_number, first_name, last_name, card_pan) VALUES(1234567894, 'Fireboy','Waterili','1234-****-****-3450');
 
-CREATE TABLE allowed_peer_ip (
-	realm varchar(127) default '',
-	ip_range varchar(256),
-	primary key (realm,ip_range)
-);
-
-CREATE TABLE denied_peer_ip (
-	realm varchar(127) default '',
-	ip_range varchar(256),
-	primary key (realm,ip_range)
-);
-
-CREATE TABLE turn_origin_to_realm (
-	origin varchar(127),
-	realm varchar(127),
-	primary key (origin)
-);
-
-CREATE TABLE turn_realm_option (
-	realm varchar(127) default '',
-	opt varchar(32),
-	value varchar(128),
-	primary key (realm,opt)
-);
-
-CREATE TABLE oauth_key (
-	kid varchar(128),
-	ikm_key varchar(256),
-	timestamp bigint default 0,
-	lifetime integer default 0,
-	as_rs_alg varchar(64) default '',
-	realm varchar(127),
-	primary key (kid)
-);
-
-CREATE TABLE admin_user (
-	name varchar(32),
-	realm varchar(127),
-	password varchar(127),
-	primary key (name)
-);
+SELECT * FROM gtworld_users;
